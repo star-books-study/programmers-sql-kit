46b2201f76de38a2fe32fb218aaf922c5ffb3384
Song-EunJu <jd06280@naver.com>
2025-01-08 21:08:39 +0900

    240108

---
+++ SELECT/은주/평균 일일 대여 요금 구하기.sql
@@ -0,0 +1,8 @@
+SELECT 
+    ROUND(SUM(DAILY_FEE) / COUNT(*), 0) AS AVERAGE_FEE
+FROM CAR_RENTAL_COMPANY_CAR
+WHERE CAR_TYPE = 'SUV'
+
+-- 한줄평
+-- 1. 반올림 함수 기억안났는데 인출 성공함
+-- 2. 평균 구하는게 있었나? 하다가 그냥 sum + count 로 했는데 AVG 가 있었네
