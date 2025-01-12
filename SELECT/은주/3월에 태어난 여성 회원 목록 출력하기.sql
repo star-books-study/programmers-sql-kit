c7bc06d9b53e946229fabb43a575df6964a47540
Song-EunJu <jd06280@naver.com>
2025-01-12 14:37:25 +0900

    250112 (2)

--- SELECT/은주/3월에 태어난 여성 회원 목록 출력하기.sql
+++ SELECT/은주/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -12,8 +12,9 @@ ORDER BY MEMBER_ID
 
 /*
 ✅ 스스로 풀었는가?
+☑️ 소요 시간 : 10분
 
-✅ 한줄평
+✅ 리뷰
 1. is not null 을 != 요따구로 썼음
 2. DATE_OF_BIRTH 에서 연월일만 뽑아내는 걸 어떻게 하는 지 몰라서 혹시 YEAR, MONTH, DAY 함수가 있나 해서 테스트해봤는데 있었음. 그거를 뽑아내고 '-' 로 이어서 문자열로 만들려고 했는데 문자열 잇는 방법을 모름. 그래서 혹시 CONCAT 이 있나 해봤는데 DATE_OF_BIRTH 만 넣어서 해보니까 연월일 형태로 나옴.
 
@@ -27,6 +28,4 @@ CONCAT(YEAR(DATE_OF_BIRTH), '-', MONTH(DATE_OF_BIRTH), '-', DAY(DATE_OF_BIRTH))
 3. CONCAT(DATE 타입 변수) 를 넣었을 때 시간 정보 사라진 이유
 CONCAT() 함수는 문자열 인자를 기대하기 때문에, DATE 타입의 값이 자동으로 문자열로 변환된다. 
 이 과정에서 MySQL은 DATE 값을 'YYYY-MM-DD' 형식의 문자열로 변환
-
-
-
+*/
---
+++ SELECT/은주/서울에 위치한 식당 목록 출력하기.sql
@@ -0,0 +1,45 @@
+SELECT
+    DISTINCT REST.REST_ID,
+    REST_NAME,
+    FOOD_TYPE,
+    FAVORITES,
+    ADDRESS,
+    (
+        SELECT ROUND(AVG(REVIEW_SCORE), 2)
+        FROM REST_REVIEW
+        WHERE REST_REVIEW.REST_ID = REST.REST_ID
+    ) AS SCORE
+FROM REST_INFO AS REST
+JOIN REST_REVIEW AS REVIEW
+    ON REST.REST_ID = REVIEW.REST_ID
+WHERE 1=1
+    AND ADDRESS LIKE '서울%'
+ORDER BY SCORE DESC, FAVORITES DESC
+
+/*
+🔼 스스로 풀었는가?
+☑️ 소요 시간 : 27분
+
+✅ 리뷰
+1. '서울' 이 포함된 문자열을 어떻게 해야하는 지 몰라서 contains, in 이런거 다써보다가 결국 검색해봄. 검색하고보니 알던 거라 아쉽..  +) LEFT(I.ADDRESS,2) = '서울' 이렇게도 처리 가능.
+2. 처음에 암 생각없이 아래처럼 짰는데 이렇게 짜면 AVG 를 계산할 때 각 식당별 리뷰 점수의 평균을 계산하는게 아니라, 전체 리뷰 평균을 계산하게 됨.
+
+SELECT
+    REST.REST_ID,
+    REST_NAME,
+    FOOD_TYPE,
+    ADDRESS,
+    ROUND(AVG(REVIEW_SCORE), 2) AS SCORE
+FROM REST_INFO AS REST
+JOIN REST_REVIEW AS REVIEW
+    ON REST.REST_ID = REVIEW.REST_ID
+WHERE ADDRESS LIKE '서울%'
+ORDER BY SCORE DESC, FAVORITES DESC
+
+따라서 식당별 리뷰 평균 점수 계산하려면 `GROUP BY REVIEW.REST_ID` 추가해주어야 함.
+group by 를 생각도 안했던 터라 그냥 서브쿼리로 review 테이블에서 평균점수 계산한 거를 출력값에 넣어줌
+이렇게 하면 REST_ID 에 여러 개의 리뷰가 있는 경우 행이 여러개 나오니까 DISTINCT 로 중복 제거해줌
+
+✅ 알아두기
+1. LIKE 검색
+*/
