e136552ab7ad7c0f639467062d8e05f84ae9160b
Song-EunJu <jd06280@naver.com>
2025-01-18 15:39:19 +0900

    250118

---
+++ SELECT/은주/강원도에 위치한 생산공장 목록 출력하기.sql
@@ -0,0 +1,18 @@
+SELECT
+   FACTORY_ID,
+   FACTORY_NAME,
+   ADDRESS
+FROM FOOD_FACTORY
+WHERE ADDRESS LIKE '강원도%'
+ORDER BY FACTORY_ID
+
+/* 
+✅ 스스로 풀었는가?
+☑️ 소요 시간 : 1분
+
+✅ 리뷰
+-
+
+✅ 알아두기
+-
+*/
---
+++ SELECT/은주/인기있는 아이스크림.sql
@@ -0,0 +1,14 @@
+SELECT FLAVOR
+FROM FIRST_HALF
+ORDER BY TOTAL_ORDER DESC, SHIPMENT_ID
+
+/* 
+✅ 스스로 풀었는가?
+☑️ 소요 시간 : 15초...?
+
+✅ 리뷰
+-
+
+✅ 알아두기
+-
+*/
