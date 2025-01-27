b0ba397b8c305d3a869410fb7f7779cc4b86d2a8
Song-EunJu <jd06280@naver.com>
2025-01-27 14:09:53 +0900

    250127

---
+++ SELECT/은주/모든 레코드 조회하기.sql
@@ -0,0 +1,20 @@
+SELECT
+    ANIMAL_ID,
+    ANIMAL_TYPE,
+    DATETIME,
+    INTAKE_CONDITION,
+    NAME,
+    SEX_UPON_INTAKE
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID
+
+/* 
+✅ 스스로 풀었는가?
+☑️ 소요 시간 : 1분
+
+✅ 리뷰
+-
+
+✅ 알아두기
+-  
+*/
