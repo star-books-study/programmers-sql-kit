dd58cf9ea43231157b574f6a906a925280628ebb
Song-EunJu <jd06280@naver.com>
2025-02-23 23:02:45 +0900

    250223

---
+++ SELECT/은주/Python 개발자 찾기.sql
@@ -0,0 +1,19 @@
+SELECT
+    ID,
+    EMAIL,
+    FIRST_NAME,
+    LAST_NAME
+FROM DEVELOPER_INFOS
+WHERE 'Python' IN (SKILL_1, SKILL_2, SKILL_3)
+ORDER BY ID
+
+/* 
+✅ 스스로 풀었는가?
+☑️ 소요 시간 : 1분
+
+✅ 리뷰
+-
+
+✅ 알아두기
+- 
+*/
