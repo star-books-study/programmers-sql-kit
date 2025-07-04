71ccba6caf683a9086dbafc43019800c966b19c0
Song-EunJu <jd06280@naver.com>
2025-07-05 01:02:03 +0900

    250705

---
+++ JOIN/은주/없어진 기록 찾기.sql
@@ -0,0 +1,17 @@
+SELECT
+    AO.ANIMAL_ID,
+    AO.NAME
+FROM ANIMAL_OUTS AO
+LEFT JOIN ANIMAL_INS AI ON AO.ANIMAL_ID = AI.ANIMAL_ID 
+WHERE AI.ANIMAL_ID IS NULL
+
+/* 
+✅ 스스로 풀었는가?
+☑️ 소요 시간 : 4분
+
+✅ 리뷰
+-
+
+✅ 알아두기
+-
+*/
