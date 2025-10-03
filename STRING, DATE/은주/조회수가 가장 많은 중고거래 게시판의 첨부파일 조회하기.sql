87070a432c77de007bfa990fc312a60e46410e76
Song-EunJu <jd06280@naver.com>
2025-10-03 23:11:51 +0900

    251003

---
+++ STRING, DATE/은주/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,21 @@
+SELECT 
+    CONCAT('/home/grep/src/', UGB.BOARD_ID, '/', FILE_ID, FILE_NAME, FILE_EXT) AS FILE_PATH
+FROM USED_GOODS_BOARD UGB
+JOIN USED_GOODS_FILE UGF
+    ON UGB.BOARD_ID = UGF.BOARD_ID
+WHERE UGB.VIEWS = (
+    SELECT MAX(VIEWS)
+    FROM USED_GOODS_BOARD
+)
+ORDER BY FILE_ID DESC
+
+/* 
+✅ 스스로 풀었는가?
+☑️ 소요 시간 : 6분
+
+✅ 리뷰
+- 
+
+✅ 알아두기
+- 
+*/
